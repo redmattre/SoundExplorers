4b0f9f2490dff6799b26930971f142cadceceb2d
redmattre <65075982+redmattre@users.noreply.github.com>
2025-01-22 19:45:32 +0100

    firstdraft

---
+++ scroll.js
@@ -0,0 +1,17 @@
+const footer = document.getElementById('footer');
+
+window.addEventListener('scroll', function () {
+  // Ottieni la posizione corrente e l'altezza totale della pagina
+  const scrollTop = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const documentHeight = document.documentElement.scrollHeight;
+
+  // Controlla se l'utente ha raggiunto la fine della pagina
+  if (scrollTop + windowHeight >= documentHeight) {
+    footer.style.opacity = '1';
+    // footer.style.display = 'block';
+  } else {
+    footer.style.opacity = '0';
+    // footer.style.display = 'none';
+  }
+});
